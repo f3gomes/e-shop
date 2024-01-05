c1a8c9f9dd61449cbeac52d423d0bbbcc66962d2
fomes <fgomesdeluna@gmail.com>
2024-01-05 16:39:00 -0300

    creates banner demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Container } from "@/components/Container";
+import { HomeBanner } from "@/components/HomeBanner";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+      </Container>
+    </div>
+  );
 }
