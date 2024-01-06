e32cef1273121bbfc6018d4ac9a519b78bf4a4c8
fomes <fgomesdeluna@gmail.com>
2024-01-06 06:46:32 -0300

    Update custom banner

--- components/HomeBanner/index.tsx
+++ components/HomeBanner/index.tsx
@@ -1,3 +1,4 @@
+import { shopInfo } from "@/shop-info/data";
 import Image from "next/image";
 import { ReactNode } from "react";
 
@@ -11,13 +12,13 @@ export function HomeBanner({ children }: HomeBannerProps) {
       <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
         <div className="mb-8 md:mb-0 text-center select-none">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">
-            Summer Sale
+            {shopInfo.promo.title}
           </h1>
           <p className="text-lg md:text-xl text-white mb-2">
-            Enjoy discounts on selected items
+            {shopInfo.promo.description}
           </p>
           <p className="text-2xl md:text-5xl text-yellow-400 font-bold">
-            GET 50% OFF
+            {shopInfo.promo.discountMessage}
           </p>
         </div>
 
--- shop-info/data.ts
+++ shop-info/data.ts
@@ -1,4 +1,10 @@
 export const shopInfo = {
   name: "E-shop",
   greeting: "Welcome to my shop!",
+
+  promo: {
+    title: "Summer Sale",
+    description: "Enjoy discounts on selected items",
+    discountMessage: "GET 50% OFF",
+  },
 };
