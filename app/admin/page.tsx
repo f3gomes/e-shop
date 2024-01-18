d0888f64bfe4982fdee9a38eb3250eaf8461ade9
fomes <fgomesdeluna@gmail.com>
2024-01-18 13:53:55 -0300

    remove chart lib

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -4,25 +4,17 @@ import getUsers from "@/actions/getUsers";
 import getOrders from "@/actions/getOrders";
 import getProducts from "@/actions/getProducts";
 import { Container } from "@/components/Container";
-
 import Summary from "./Summary";
-import BarGraph from "./BarGraph";
-import getGraphData from "@/actions/getGraphData";
 
 export default async function AdminPage() {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
 
-  const graphData = await getGraphData();
-
   return (
     <div>
       <Container>
         <Summary products={products} orders={orders} users={users} />
-        <div className="mt-10 mx-auto max-w-[1150px]">
-          <BarGraph data={graphData} />
-        </div>
       </Container>
     </div>
   );
