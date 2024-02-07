cee3eb813451032d10262e4ed5cae4513b834c72
fomes <fgomesdeluna@gmail.com>
2024-02-07 14:51:27 -0300

    include loading spinner on navigation

---
+++ app/admin/manage-products/loading.tsx
@@ -0,0 +1,6 @@
+import LoadingPage from "@/components/LoadingPage";
+import React from "react";
+
+export default function Loading() {
+  return <LoadingPage />;
+}
---
+++ components/LoadingPage/index.tsx
@@ -0,0 +1,9 @@
+import { CircularProgress } from "@mui/material";
+
+export default function LoadingPage() {
+  return (
+    <div className="w-full text-center mt-28">
+      <CircularProgress size={60} />
+    </div>
+  );
+}
